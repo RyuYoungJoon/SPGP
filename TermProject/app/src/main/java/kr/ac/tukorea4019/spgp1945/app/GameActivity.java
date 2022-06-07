8aeab1150a95366d24a4713aed754d18165af5b8
RyuYoungJoon <74610708+RyuYoungJoon@users.noreply.github.com>
2022-06-07 21:50:08 +0900

    Activityxml 레이아웃 만들기

--- TermProject/app/src/main/java/kr/ac/tukorea4019/spgp1945/app/GameActivity.java
+++ TermProject/app/src/main/java/kr/ac/tukorea4019/spgp1945/app/GameActivity.java
@@ -1,2 +1,43 @@
-package kr.ac.tukorea4019.spgp1945.app;public class GameActivity {
+package kr.ac.tukorea4019.spgp1945.app;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import kr.ac.tukorea4019.spgp1945.framework.view.GameView;
+import kr.ac.tukorea4019.spgp1945.framework.game.BaseGame;
+import kr.ac.tukorea4019.spgp1945.game.MainGame;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MainGame.get();
+        setContentView(new GameView(this, null));
+    }
+
+    @Override
+    protected void onPause() {
+        GameView.view.pauseGame();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        GameView.view.resumeGame();
+    }
+
+    @Override
+    protected void onDestroy() {
+        GameView.view = null;
+        BaseGame.clear();
+        super.onDestroy();
+    }
+
+
+
+
 }
