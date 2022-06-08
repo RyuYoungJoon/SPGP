122fa89bb91f5c89586879734ac47f0e9a9ae5ce
RyuYoungJoon <74610708+RyuYoungJoon@users.noreply.github.com>
2022-06-08 16:40:53 +0900

    game 부분 메인게임과 총알, 충돌처리

---
+++ TermProject/app/src/main/java/kr/ac/tukorea4019/spgp1945/game/CollisionChecker.java
@@ -0,0 +1,54 @@
+package kr.ac.tukorea4019.spgp1945.game;
+
+
+import android.graphics.Canvas;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+import kr.ac.tukorea4019.spgp1945.framework.util.CollisionHelper;
+import kr.ac.tukorea4019.spgp1945.framework.interfaces.GameObject;
+
+public class CollisionChecker implements GameObject {
+    private static final String TAG = CollisionChecker.class.getSimpleName();
+
+    @Override
+    public void update() {
+        MainGame game = MainGame.get();
+        ArrayList<GameObject> bullets = game.objectsAt(MainGame.Layer.bullet);
+        ArrayList<GameObject> enemies = game.objectsAt(MainGame.Layer.enemy);
+        for (GameObject o1 : enemies) {
+            if (!(o1 instanceof Enemy)) {
+                continue;
+            }
+            Enemy enemy = (Enemy) o1;
+            boolean collided = false;
+            for (GameObject o2 : bullets) {
+                if (!(o2 instanceof Bullet)) {
+                    continue;
+                }
+                Bullet bullet = (Bullet) o2;
+                if (CollisionHelper.collides(enemy, bullet)) {
+                    Log.d(TAG, "Collision !!");
+                    game.remove(bullet);
+                    boolean dead = enemy.decreaseLife(bullet.getPower());
+                    if (dead) {
+                        game.remove(enemy);
+                        game.score.add(enemy.getScore());
+                    }
+                    collided = true;
+                    break;
+                }
+            }
+            if (collided) {
+                continue;
+            }
+            // check enemy vs fighter
+        }
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+}
